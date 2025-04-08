9e4248057e7ae7868aaa0f47bd222777c27a814f
Varun <varun.ps@codenish.com>
2025-04-08 19:24:34 +0530

    init

--- home/urls.py
+++ home/urls.py
@@ -4,7 +4,7 @@ from home.views import *
 urlpatterns = [
     path('', index, name="index"),
     path('search/', product_search, name='product_search'),
-    path('contact/', contacts, name='contact'),
+    path('contact/', contact, name='contact'),
     path('about/', about, name='about'),
     path('terms-and-conditions/', terms_and_conditions, name='terms-and-conditions'),
     path('privacy-policy/', privacy_policy, name='privacy-policy'),
